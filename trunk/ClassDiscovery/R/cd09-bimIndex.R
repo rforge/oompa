e0c4d8f2b1819fd50f208f272f7ebe84f01cf303
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-20 22:30:47 +0000

    Added the bimodal index code along with a sample data set
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/ClassDiscovery/R/cd09-bimIndex.R
@@ -0,0 +1,23 @@
+## bimIndex.R
+
+bimodalIndex <- function(dataset, verbose=TRUE) {
+  bim <- matrix(NA, nrow=nrow(dataset), ncol=6)
+  if (verbose) cat("1 ")
+  for (i in 1:nrow(dataset)) {
+    if (verbose && 0 == i%%100) cat(".")
+    if (verbose && 0 == i%%1000) cat(paste("\n", 1 + i/1000, ' ', sep=''))
+    x <- as.vector(as.matrix(dataset[i, ]))
+    if (any(is.na(x))) next
+    mc <- Mclust(x, G = 2, modelNames = "E")
+    sigma <- sqrt(mc$parameters$variance$sigmasq)
+    delta <- abs(diff(mc$parameters$mean))/sigma
+    pi <- max(mc$parameters$pro)
+    bi <- delta * sqrt(pi*(1-pi))
+    bim[i,] <-  c(mc$parameters$mean, sigma=sigma, delta=delta, pi=pi, bim=bi)
+  }
+  if(verbose) cat("\n")
+  bim <- as.data.frame(bim)
+  dimnames(bim) <- list(rownames(dataset),
+                        c("mu1", "mu2", "sigma", "pi", "delta", "BI"))
+  bim
+}
