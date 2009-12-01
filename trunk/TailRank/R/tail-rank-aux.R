973f4e89aaccbaaefc07b3d76a68aa0b6d97242c
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-01 17:07:08 +0000

    Moved matrixMean and matrixVar code from their (duplicated) positions in ClassComparison and TailRank into oompaBase
    checked in separately to avoid an apparent bug in subversion
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/TailRank/R/tail-rank-aux.R
+++ trunk/TailRank/R/tail-rank-aux.R
@@ -3,22 +3,6 @@
 # Copyright, Kevin R. Coombes, 2004
 
 ############################
-# vectorized functions to compute row-wise mean and variance quickly
-matrixMean <- function(x) {
-  x <- as.matrix(x)
-  n <- ncol(x)
-  x %*% matrix(rep(1, n), nrow=n)/n
-}
-
-matrixVar <- function(x, xmean) {
-  x <- as.matrix(x)
-  n <- ncol(x)
-  v <- (((x * x) %*% matrix(rep(1, n), nrow=n)) - n * xmean * xmean)/(n - 1)
-  v[v < 0] <- 0
-  v
-}
-
-############################
 # Finally, we include a function to include tolerance bounds on the
 # quantiles of a normal distribution. The reference for this function
 # is the National Bureau of Standards handbook by Natrella, 1963.
