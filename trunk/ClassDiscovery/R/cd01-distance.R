13036ca9da796b14b24eae0a7d91c095464a06a7
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-24 23:21:32 +0000

    Added "pcc" function to the ClassDiscovery package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/ClassDiscovery/R/cd01-distance.R
+++ trunk/ClassDiscovery/R/cd01-distance.R
@@ -55,3 +55,57 @@ plotColoredClusters <- function(hd, labs, cols, cex=0.8, main='', line=0, ...)
   mtext(labs, side=1, line=line, at=order(hd$order), col=cols, las=2, cex=cex)
 }
 
+
+
+pcc <- function(hd, colors=NULL, ...) {
+  opar <- par(no.readonly=TRUE)
+  on.exit(par(opar))
+  if (!is.null(colors)) {
+    L <- length(colors)
+    lwid <- c(6, 1)
+    lhei <- c(6, rep(0.5, L))
+    lmat <- matrix(c(L+1, 1:L, L+2, rep(0,L)), ncol=2)
+    layout(lmat, widths=lwid, heights=lhei, respect=FALSE)
+    for (i in 1:L) {
+      par(mar = c(0.5, 0.5, 0, 0))
+      x <- colors[[i]]
+      image(matrix(as.numeric(x$fac)[hd$order], ncol=1), col = x$col,
+            axes = FALSE, xaxt='n', yaxt='n', xlab='', ylab='')
+      mtext(side = 4, names(colors)[i], line = 0.5, las=1)
+    }
+    par(mar=c(5, 0.5, 7, 0))
+  }
+  plot(as.dendrogram(hd), yaxt='n', xaxs='i', ...)
+  if (!is.null(colors)) {
+    L <- length(colors)
+    N <- sum(J <- unlist(lapply(colors, function(x) length(levels(x$fac)))))
+    plot(c(0,1), c(1, N+L+1), type='n', xlab='', xaxt='n', ylab='', yaxt='n')
+    for (i in 1:length(colors)) {
+      y <- i + cumsum(J)[i]
+      text(0.1, y+0.5, names(J)[i], adj=0)
+      legend(0.3, y, levels(colors[[i]]$fac), col=colors[[i]]$col, pch=15)
+    }
+  } else {
+    plot(c(0,1), c(0,1), type='n', xlab='', xaxt='n', ylab='', yaxt='n')
+  }
+  layout(matrix(1), widths=1, heights=1)
+  invisible(hd)
+}
+
+if(FALSE) {
+  fakedata <- matrix(rnorm(200*30), ncol=30)
+  colnames(fakedata) <- paste("P", 1:30, sep='')
+
+  faccol <- list(fac=factor(rep(c("A", "B"), each=15)),
+                 col=c(A='red', B='green'))
+  fac2col <- list(fac=factor(rep(c("X", "Y", "Z"), times=10)),
+                 col=c(X='cyan', Y='orange', Z='purple'))
+  BA <- faccol
+  BA$col <- c(A='blue', B='yellow')
+  colors <- list(AB=faccol, XYZ=fac2col, "tricky long name"=fac2col,
+                 another=BA)
+
+  hc <- hclust(distanceMatrix(fakedata, "pearson"), "ward")
+
+  pcc(hc, colors)
+}
