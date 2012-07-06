068f7e1bbd094ebe191f673382c599fefb938e05
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-06 20:36:33 +0000

    moved test code for ClassDiscovery from /inst/samples to /tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@150 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/ClassDiscovery/tests/test04.1-gpca.R
@@ -0,0 +1,14 @@
+# simulate samples from thre different groups, with generic genes
+d1 <- matrix(rnorm(100*10, rnorm(100, 0.5)), nrow=100, ncol=10, byrow=FALSE)
+d2 <- matrix(rnorm(100*10, rnorm(100, 0.5)), nrow=100, ncol=10, byrow=FALSE)
+d3 <- matrix(rnorm(100*10, rnorm(100, 0.5)), nrow=100, ncol=10, byrow=FALSE)
+dd <- cbind(d1, d2, d3)
+
+# perform PCA in gene space
+gpc <- GenePCA(dd)
+
+# plot the results
+plot(gpc)
+
+# cleanup
+rm(d1, d2, d3, dd, gpc)
