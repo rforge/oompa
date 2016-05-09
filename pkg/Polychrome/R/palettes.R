ac8bcb26de906af7864a7e3935472548bbff9b7e
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-09 12:42:11 +0000

    Started restructuring palettes to be more consistent (and to get rid of a couple of whining complaints from R CMD check)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@268 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Polychrome/R/palettes.R
+++ pkg/Polychrome/R/palettes.R
@@ -1,3 +1,17 @@
+isccNames <- function(colorset) {
+  data("iscc", package="Polychrome", envir=environment())
+  iscc <- get("iscc", envir=environment())
+  munsell <- as(hex2RGB(iscc$Hex), "LUV")
+  d3 <- function(y0) {
+    temp <- sweep(munsell@coords, 2, y0, "-")
+    dist <- apply(temp^2, 1, sum)
+    which(dist==min(dist))
+  }
+  alpha <- as(hex2RGB(colorset), "LUV")
+  m <- apply(alpha@coords, 1, d3)
+  iscc$longName[m]
+}
+
 makePalette <- function(n, colorset) {
   if (n < 3) {
     warning("minimal value for n is 3, returning requested palette with 3 levels\n")
@@ -12,6 +26,12 @@ makePalette <- function(n, colorset) {
     n <- L
   }
   colorset[1:L]
+}
+####################################
+palette36.colors <- function(n=36) {
+  data("palette36", package="Polychrome", envir=environment())
+  palette36 <- get("palette36", envir=environment())
+  return (makePalette(n, palette36))
 }
 
 ####################################
@@ -84,85 +104,4 @@ green.armytage.colors <- function(n=26) {
   makePalette(n, green.armytage)
 }
 
-####################################
-# alphacolors
 
-## should not be called by user code
-createnew36 <- function() {
-  # three seed colors
-  ebony <- hex(LUV(30,0,0))
-  iron <- hex(LUV(90, 0, 0))
-  red <- hex(RGB(0.8, 0, 0))
-  # set random number seed for reproducibility. 
-  set.seed(567629)
-  createPalette(36, c(ebony, iron, red))
-}
-new36colors <- createnew36()
-createAlphabet <- function() {
-  alphabet <- new36colors[1:26]
-  # want to assign not-completely-stupid names
-  # start with names from Green-Armytage
-  colsch <- green.armytage.colors()
-  ga <- as(hex2RGB(colsch), "LUV")
-  alpha <- as(hex2RGB(alphabet), "LUV")
-  d3 <- function(y0) {
-    temp <- sweep(ga@coords, 2, y0, "-")
-    dist <- apply(temp^2, 1, sum)
-    which(dist==min(dist))
-  }
-  m <- apply(alpha@coords, 1, d3)
-  odd <- which(!(1:26 %in% m))
-  m[duplicated(m)] <- odd
-  names(alphabet) <- names(colsch)[m]
-  # now manually swap things around
-  b <- which(names(alphabet)=='blue')
-  v <- which(names(alphabet)=='violet')
-  tu <- which(names(alphabet)=='turquoise')
-  names(alphabet)[b] <- "turquoise"
-  names(alphabet)[tu] <- "violet"
-  names(alphabet)[v] <- "blue"
-  rm(b, v, tu)
-  s <- which(names(alphabet) == "sky")
-  g <- which(names(alphabet) == "green")
-  j <- which(names(alphabet) == "jade")
-  k <- which(names(alphabet) == "khaki")
-  o <- which(names(alphabet) == "orpiment")
-  w <- which(names(alphabet) == "wine")
-  names(alphabet)[g] <- "sea"
-  names(alphabet)[k] <- "jade"
-  names(alphabet)[o] <- "green"
-  names(alphabet)[s] <- "wine"
-  names(alphabet)[j] <- "O"
-  names(alphabet)[w] <- "kingcrab"
-  rm(s,g,o,k, j)
-  names(alphabet)[names(alphabet)=="zinnia"] <- "orange"
-  names(alphabet)[names(alphabet)=="lime"] <- "zinnia"
-  names(alphabet)[names(alphabet)=="amethyst"] <- "lavender"
-  names(alphabet)[names(alphabet)=="O"] <- "amethyst"
-  names(alphabet)[names(alphabet)=="mallow"] <- "magenta"
-  names(alphabet)[names(alphabet)=="damson"] <- "ultraviolet"
-  names(alphabet)[names(alphabet)=="uranium"] <- "damson"
-  x <- which(names(alphabet) == "xanthin")
-  h <- which(names(alphabet) == "honeydew")
-  names(alphabet)[x] <- "honey"
-  names(alphabet)[h] <- "xanthin"
-  rm(x,h)
-  alphabet["ebony"] <- "#565656"
-  alphabet["iron"] <- "#E2E2E2"
-  alphabet
-}
-
-isccNames <- function(colorset) {
-  data('iscc', package='Polychrome')
-  munsell <- as(hex2RGB(iscc$Hex), "LUV")
-  d3 <- function(y0) {
-    temp <- sweep(munsell@coords, 2, y0, "-")
-    dist <- apply(temp^2, 1, sum)
-    which(dist==min(dist))
-  }
-  alpha <- as(hex2RGB(colorset), "LUV")
-  m <- apply(alpha@coords, 1, d3)
-  iscc$longName[m]
-}
-
-names(new36colors) <- isccNames(new36colors)
