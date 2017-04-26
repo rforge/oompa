a291b723c03a12f8ff31e90b4d4c0b3e9b89c043
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-04-26 12:31:32 +0000

    Tools update.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@327 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tools/prep.R
+++ tools/prep.R
@@ -1,12 +1,45 @@
 source("https://bioconductor.org/biocLite.R")
-biocLite()
+if (!require("Biobase")) {
+  biocLite()
+}
+mybiocs <- c("edgeR",
+             "Biostrings")
+for (p in mybiocs) {
+  if (!require(p, character.only=TRUE)) {
+    biocLite(p)
+  }
+}
 
-install.packages(c("knitr",
-                   "rmarkdown",
-                   "RColorBrewer",
-                   "colorspace",
-                   "rgl",
-                   "vioplot",
-                   "xtable",
-                   "mclust"
-                   ))
+mypacks <- c("knitr",
+             "rmarkdown",
+             "RColorBrewer",
+             "colorspace",
+             "rgl",
+             "vioplot",
+             "xtable",
+             "mclust",
+             "e1071",
+             "randomForest",
+             "neuralnet",
+             "mc2d",
+             "combinat",
+             "abind",
+             "ltm",
+             "foreach",
+             "doParallel",
+             "KernSmooth",
+             "matrixStats",
+             "kernlab",
+             "changepoint",
+             "cpm",
+             "movMF",
+             "ade4",
+             "mgcv",
+             "quantreg",
+             "robustbase"
+             )
+for (p in mypacks) {
+  if (!require(p, character.only=TRUE)) {
+    install.packages(p)
+  }
+}
