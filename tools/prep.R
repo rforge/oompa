05c91b8fd946bee3384de2ac30c30401316ab1dd
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-04-26 11:23:33 +0000

    tools update.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@326 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tools/prep.R
@@ -0,0 +1,12 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+
+install.packages(c("knitr",
+                   "rmarkdown",
+                   "RColorBrewer",
+                   "colorspace",
+                   "rgl",
+                   "vioplot",
+                   "xtable",
+                   "mclust"
+                   ))
