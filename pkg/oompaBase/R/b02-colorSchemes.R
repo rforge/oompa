3d77dabe20bdd69ace7302683e2f2cc131423f48
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-08-19 17:48:24 +0000

    Added the "wheel" color scheme, with any level of saturation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@446 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/oompaBase/R/b02-colorSchemes.R
+++ pkg/oompaBase/R/b02-colorSchemes.R
@@ -59,7 +59,17 @@ jetColors <- function(N){
 
   ## assemble everything last value is returned
   rgb(temp.rgb[,1], temp.rgb[,2], temp.rgb[,3])
+}
 
+# Stack Overflow
+desat <- function(cols, sat=0.5) {
+    X <- diag(c(1, sat, 1)) %*% rgb2hsv(col2rgb(cols))
+    hsv(X[1,], X[2,], X[3,])
+}
+
+wheel <- function(N, sat = 1) {
+  A <- rev(c("red", "yellow", "green", "cyan", "blue", "magenta"))
+  colorRampPalette(desat(A, sat))(N)
 }
 
 ################################################################
